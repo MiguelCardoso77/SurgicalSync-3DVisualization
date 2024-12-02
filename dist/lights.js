fa213290fcb3a9be13f388110f16a356a4c92b75
DiogoMPR <diogo.mp.ribeiro2004@gmail.com>
2024-12-02 12:34:21 +0000

    Adding more points of light, one per bed

--- dist/lights.js
+++ dist/lights.js
@@ -47,6 +47,54 @@ export default class Lights {
         this.object.pointLight2.shadow.camera.far = 15.0;
         this.object.add(this.object.pointLight2);
 
+        // Create the third point light and turn on shadows for this light
+        this.object.pointLight3 = new THREE.PointLight(0xDCDCDC, this.pointLight3.intensity, this.pointLight3.distance);
+        this.object.pointLight3.position.set(this.pointLight3.position.x, this.pointLight3.position.y, this.pointLight3.position.z);
+        this.object.pointLight3.castShadow = true;
+
+        // Set up shadow properties for this light
+        this.object.pointLight3.shadow.mapSize.width = 256;
+        this.object.pointLight3.shadow.mapSize.height = 256;
+        this.object.pointLight3.shadow.camera.near = 5.0;
+        this.object.pointLight3.shadow.camera.far = 15.0;
+        this.object.add(this.object.pointLight3);
+
+        // Create the fourth point light and turn on shadows for this light
+        this.object.pointLight4 = new THREE.PointLight(0xDCDCDC, this.pointLight4.intensity, this.pointLight4.distance);
+        this.object.pointLight4.position.set(this.pointLight4.position.x, this.pointLight4.position.y, this.pointLight4.position.z);
+        this.object.pointLight4.castShadow = true;
+
+        // Set up shadow properties for this light
+        this.object.pointLight4.shadow.mapSize.width = 256;
+        this.object.pointLight4.shadow.mapSize.height = 256;
+        this.object.pointLight4.shadow.camera.near = 5.0;
+        this.object.pointLight4.shadow.camera.far = 15.0;
+        this.object.add(this.object.pointLight4);
+
+        // Create the fifth point light and turn on shadows for this light
+        this.object.pointLight5 = new THREE.PointLight(0xDCDCDC, this.pointLight5.intensity, this.pointLight5.distance);
+        this.object.pointLight5.position.set(this.pointLight5.position.x, this.pointLight5.position.y, this.pointLight5.position.z);
+        this.object.pointLight5.castShadow = true;
+
+        // Set up shadow properties for this light
+        this.object.pointLight5.shadow.mapSize.width = 256;
+        this.object.pointLight5.shadow.mapSize.height = 256;
+        this.object.pointLight5.shadow.camera.near = 5.0;
+        this.object.pointLight5.shadow.camera.far = 15.0;
+        this.object.add(this.object.pointLight5);
+
+        // Create the sixth point light and turn on shadows for this light
+        this.object.pointLight6 = new THREE.PointLight(0xDCDCDC, this.pointLight6.intensity, this.pointLight6.distance);
+        this.object.pointLight6.position.set(this.pointLight6.position.x, this.pointLight6.position.y, this.pointLight6.position.z);
+        this.object.pointLight6.castShadow = true;
+
+        // Set up shadow properties for this light
+        this.object.pointLight6.shadow.mapSize.width = 256;
+        this.object.pointLight6.shadow.mapSize.height = 256;
+        this.object.pointLight6.shadow.camera.near = 5.0;
+        this.object.pointLight6.shadow.camera.far = 15.0;
+        this.object.add(this.object.pointLight6);
+
         this.object.traverse((child) => {
             if (child.isLight) {
                 child.frustumCulled = true;
